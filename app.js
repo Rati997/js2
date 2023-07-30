c30c2b9bd5443652c27850f21630ebbd22ebaef4
rati <rmekokishvili3gmail.com>
2023-07-31 00:28:58 +0400

    first commit

---
+++ app.js
@@ -0,0 +1,48 @@
+
+// assignment2
+
+let a = 1, b = 1;
+let c = ++a;
+let d =c++;
+
+alert(a); //1
+alert(c); //2
+alert(d++); //2
+
+
+//assignment 3
+
+
+alert(5 > 4); // true 5 metia 4ze
+alert("apple" > "pineapple"); // false meti romelshic weria is unda iyos meti(pineapple meti asosgan shedgeba)
+alert("2" > "12"); //false 2 ani meti ar aris tormetze, stringebi chvelebriv cifrebad unda warmoviginot
+alert(undefined == null); // true eseni nolois toli xdeba 
+alert(undefined === null); //false. gio es folsia ragaca ucnauris gamo :D statias velodebit wasakitxad
+alert(null == " 0 ") //false
+alert(null === " 0 ") // false.  mkacri shemowmebis gamo xdeba Shedareba tipis da nishnis amis gamo 0 ani gardaiqmneba 1-ad
+
+
+
+//assignment 4
+
+
+alert(null || 2 || undefined); //2
+alert(1 && null && 2); //null
+alert(null || 2 && 3 || 4); //3
+alert(!1 && !2 ||3); //3
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
